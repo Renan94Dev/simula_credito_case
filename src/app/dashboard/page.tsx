8bb6b39b6f61064ba755ef8cbaf972a0c766f438
Renan Dev <rodilon10@gmail.com>
2025-11-23 07:53:31 -0300

    feat: improve dashboard bank card UI with hover effect and separator

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -22,7 +22,7 @@ export default function Dashboard() {
 						{BankCard.map((bank) => (
 							<div
 								key={Math.random() * 100}
-								className="flex-1 border border-gray-200 rounded-md p-4 space-y-2 shadow-md cursor-pointer"
+								className="flex-1 border border-gray-200 rounded-md p-4 space-y-2 shadow-md cursor-pointer group"
 							>
 								<div className="flex items-center justify-between">
 									<div className="flex flex-col gap-2">
@@ -37,6 +37,7 @@ export default function Dashboard() {
 
 										<span className="flex gap-2 text-gray-500">
 											<small>a.m: {bank.am}%</small>
+											<small>|</small>
 											<small>a.a: {bank.aa}%</small>
 										</span>
 									</div>
@@ -49,7 +50,7 @@ export default function Dashboard() {
 									/>
 								</div>
 
-								<div className="flex items-center gap-1 text-primary">
+								<div className="flex items-center gap-1 text-primary group-hover:underline">
 									<span>Ver detalhes</span>
 									<span>
 										<ArrowRight size={18} />
