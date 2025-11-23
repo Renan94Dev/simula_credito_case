e54a0653f7344edb7b996a8c7d861b7242639f73
Renan Dev <rodilon10@gmail.com>
2025-11-23 18:20:39 -0300

    feat: add developer attribution and link to footer

--- src/ui/footer/index.tsx
+++ src/ui/footer/index.tsx
@@ -1,7 +1,13 @@
 export const FooterContent = () => {
 	return (
 		<footer className="text-xs text-center text-gray-600">
-			Simula crédito &copy; {new Date().getFullYear()}
+			Simula crédito &copy; {new Date().getFullYear()} | Desenvolvido por{" "}
+			<a
+				href="https://www.linkedin.com/in/renan-odilon/"
+				className="text-primary underline"
+			>
+				Renan Odilon
+			</a>
 		</footer>
 	);
 };
